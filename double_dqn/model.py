7deeb080d96f750985707d38317bdafeece1ca43
SamYuen101234 <samyuan101234@gmail.com>
2022-04-04 14:55:14 +0800

    fix the bug of double dqn

--- double_dqn/model.py
+++ double_dqn/model.py
@@ -25,7 +25,7 @@ import copy
 class DinosaurNet(nn.Module):
     def __init__(self, img_channels, ACTIONS):
         super().__init__()
-        self.online = nn.Sequential(
+        '''self.online = nn.Sequential(
                 nn.Conv2d(in_channels=img_channels, out_channels=32, kernel_size=8, stride=4),
                 nn.ReLU(),
                 nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2),
@@ -34,9 +34,25 @@ class DinosaurNet(nn.Module):
                 nn.ReLU(),
                 nn.Flatten(),
                 nn.Linear(2304, 512),
+                #nn.Dropout(p=0.2),
                 nn.ReLU(),
                 nn.Linear(512, ACTIONS),
-            )
+            )'''
+        self.online = nn.Sequential(
+            nn.Conv2d(in_channels=img_channels, out_channels=32, kernel_size=8, stride=4, padding=1),
+            nn.MaxPool2d(2,2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=1),
+            nn.MaxPool2d(2,2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=2),
+            nn.MaxPool2d(2,2),
+            nn.ReLU(inplace=True),
+            nn.Flatten(),
+            nn.Linear(256, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_features=512, out_features=ACTIONS)
+        )
         
         self.target = copy.deepcopy(self.online)
         # Q_target parameters are frozen.
@@ -52,5 +68,5 @@ class DinosaurNet(nn.Module):
         elif model == "target":
             return self.target(input)     
 '''data = torch.rand(1,4,80,80)
-model = buildmodel(4, 2)
-print(model(data))'''
+model = DinosaurNet(4, 2)
+print(model(data, model ='online'))'''
