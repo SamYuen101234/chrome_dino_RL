2ccbccdf41f56fa708fac8d9399cdb4e8b424670
SamYuen101234 <samyuan101234@gmail.com>
2022-05-22 00:08:42 +0800

    add prioritized replay buffer in training

--- models/model.py
+++ models/model.py
@@ -2,9 +2,7 @@
 import torch.nn as nn
 import torch
 import copy
-
 import numpy as np
-
 from torch import optim
 
 class ChromeDinoAgent:
@@ -17,7 +15,9 @@ class ChromeDinoAgent:
         self.gamma = gamma
         self.device = device
 
+
     def sync_target(self):
+        ''' A function for double DQN'''
         pass
 
     def save_model(self):
@@ -26,29 +26,57 @@ class ChromeDinoAgent:
     def get_action(self, state):
         raise NotImplementedError
 
+    def compute_loss(self, state_t, action_t, reward_t, state_t1, terminal):
+        pass
+
     def step(self, state_t, action_t, reward_t, state_t1, terminal):
         raise NotImplementedError
-        
+
+    def last_layer(self):
+        ''' A function to retrieve the last convolutional layer of the deep model for Grad CAM visualization'''
+        raise NotImplementedError
+
+    def gradient_2norm(self):
+        ''' 
+            return the 2 norm of gradient vector
+            use this function to check the norm and fine tune the max norm of norm clipping
+        '''
+
+        for p in self.model.parameters():
+            param_norm = p.grad.data.norm(2)
+            total_norm += param_norm.item() ** 2
+        total_norm = total_norm ** (1. / 2)
+
+        return total_norm
+
+
+# Deep Q-Network
 class Baseline(ChromeDinoAgent):
 
-    def __init__(self, img_channels, ACTIONS, lr, batch_size, gamma, device):
+    def __init__(self, img_channels, ACTIONS, lr, weight_decay, batch_size, gamma, device, grad_norm_clipping):
         super().__init__(img_channels, ACTIONS, lr, batch_size, gamma, device)
         
         self.model = nn.Sequential(
                 nn.Conv2d(in_channels=img_channels, out_channels=32, kernel_size=8, stride=4),
-                nn.ReLU(),
+                nn.BatchNorm2d(32),
+                nn.ReLU(inplace=True),
                 nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2),
-                nn.ReLU(),
+                nn.BatchNorm2d(64),
+                nn.ReLU(inplace=True),
                 nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
-                nn.ReLU(),
+                nn.BatchNorm2d(64),
+                nn.ReLU(inplace=True),
                 nn.Flatten(),
+                nn.Dropout(0.2),
                 nn.Linear(2304, 512),
-                nn.ReLU(),
+                nn.Dropout(0.2),
+                nn.ReLU(inplace=True),
                 nn.Linear(512, ACTIONS),
             )
 
-        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr, weight_decay=weight_decay)
         self.criterion = nn.SmoothL1Loss()
+        self.grad_norm_clipping = grad_norm_clipping
 
     def get_action(self, state):
         with torch.no_grad():
@@ -60,7 +88,10 @@ class Baseline(ChromeDinoAgent):
     def save_model(self):
         torch.save(self.model, "./weights/baseline.pth")
 
-    def step(self, state_t, action_t, reward_t, state_t1, terminal):
+    def compute_loss(self, state_t, action_t, reward_t, state_t1, terminal):
+        pass
+
+    def step(self, state_t, action_t, reward_t, state_t1, terminal, importance=None):
         td_estimate = self.model(state_t.to(self.device))
         with torch.no_grad():
             td_target = self.model(state_t.to(self.device))
@@ -69,6 +100,10 @@ class Baseline(ChromeDinoAgent):
                     (reward_t + (1 - terminal.float())*self.gamma *torch.amax(next_Q, axis=1)).float() # put a mask on the action_t
 
         loss = self.criterion(td_estimate.to(self.device), td_target.to(self.device))
+
+        # weighted loss, if we use prioritized reply buffer
+        if importance is not None: loss = (torch.FloatTensor(importance)*loss).mean()
+
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
@@ -78,25 +113,32 @@ class Baseline(ChromeDinoAgent):
 
         return avg_loss, avg_q_max
 
+    def last_layer(self):
+        pass
+
+# Double Deep Q-Network
 class DoubleDQN(ChromeDinoAgent):
-    def __init__(self, img_channels, ACTIONS, lr, batch_size, gamma, device):
+    def __init__(self, img_channels, ACTIONS, lr, weight_decay, batch_size, gamma, device, grad_norm_clipping=10):
         super().__init__(img_channels, ACTIONS, lr, batch_size, gamma, device)
 
         self.online = nn.Sequential(
-            nn.Conv2d(in_channels=img_channels, out_channels=32, kernel_size=8, stride=4, padding=1),
-            nn.MaxPool2d(2,2),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=1),
-            nn.MaxPool2d(2,2),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=2),
-            nn.MaxPool2d(2,2),
-            nn.ReLU(inplace=True),
-            nn.Flatten(),
-            nn.Linear(256, 512),
-            nn.ReLU(inplace=True),
-            nn.Linear(in_features=512, out_features=ACTIONS)
-        )
+                #nn.BatchNorm2d(4),
+                nn.Conv2d(in_channels=img_channels, out_channels=32, kernel_size=8, stride=4),
+                nn.BatchNorm2d(32),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2),
+                nn.BatchNorm2d(64),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
+                nn.BatchNorm2d(64),
+                nn.ReLU(inplace=True),
+                nn.Flatten(),
+                nn.Dropout(0.2),
+                nn.Linear(2304, 512),
+                nn.Dropout(0.2),
+                nn.ReLU(inplace=True),
+                nn.Linear(512, ACTIONS),
+            )
         
         self.target = copy.deepcopy(self.online)
 
@@ -104,8 +146,9 @@ class DoubleDQN(ChromeDinoAgent):
         for p in self.target.parameters():
             p.requires_grad = False
 
-        self.optimizer = optim.Adam(self.online.parameters(), lr=self.lr)
+        self.optimizer = optim.Adam(self.online.parameters(), lr=self.lr, weight_decay=weight_decay)
         self.criterion = nn.SmoothL1Loss()
+        self.grad_norm_clipping = grad_norm_clipping
 
     def sync_target(self):
         self.target.load_state_dict(self.online.state_dict())
@@ -121,43 +164,42 @@ class DoubleDQN(ChromeDinoAgent):
         return action_idx
 
     def compute_loss(self, state_t, action_t, reward_t, state_t1, terminal):
+        self.online.train()
+
         td_estimate = self.online(state_t.to(self.device))[
             np.arange(0, self.batch_size), action_t
         ]  # Q_online(s,a)
+
+        self.online.eval()
+        self.target.eval()
 
         # td_target
         with torch.no_grad():
             next_state_Q = self.online(state_t1.to(self.device))
             best_action = torch.argmax(next_state_Q, axis=1)
-            next_Q = self.target(state_t1)[
+            next_Q = self.target(state_t1.to(self.device))[
                 np.arange(0, self.batch_size), best_action
             ]
             td_target = (reward_t + (1 - terminal.float())* self.gamma *next_Q).float()
         
-        return self.criterion(td_estimate.to(self.device), td_target.to(self.device))
+        return self.criterion(td_estimate.to(self.device), td_target.to(self.device)), next_Q, torch.abs(td_estimate - td_target).cpu().data.numpy()
 
-    def step(self, state_t, action_t, reward_t, state_t1, terminal):
-        # td_estimate/current Q
-        # print("Inside double dqn step, batch size: ", self.batch_size)
-        td_estimate = self.online(state_t.to(self.device))[
-            np.arange(0, self.batch_size), action_t
-        ]  # Q_online(s,a)
+    def step(self, state_t, action_t, reward_t, state_t1, terminal, importance=None):
+        # error: used for prioritized replay buffer
+        loss, next_Q, error = self.compute_loss(state_t, action_t, reward_t, state_t1, terminal)
 
-        # td_target
-        with torch.no_grad():
-            next_state_Q = self.online(state_t1.to(self.device))
-            best_action = torch.argmax(next_state_Q, axis=1)
-            next_Q = self.target(state_t1)[
-                np.arange(0, self.batch_size), best_action
-            ]
-            td_target = (reward_t + (1 - terminal.float())* self.gamma *next_Q).float()
+        # weighted loss, if we use prioritized reply buffer
+        if importance is not None: loss = (importance*loss).mean()
 
-        loss = self.criterion(td_estimate.to(self.device), td_target.to(self.device))
         self.optimizer.zero_grad()
         loss.backward()
+        nn.utils.clip_grad_norm_(self.online.parameters(), max_norm=self.grad_norm_clipping)
         self.optimizer.step()
 
         avg_loss = loss.detach().cpu().item()
         avg_q_max = torch.mean(torch.amax(next_Q)).detach().cpu().item()
 
-        return avg_loss, avg_q_max
+        return avg_loss, avg_q_max, error
+
+    def last_layer(self):
+        pass
--- models/train.py
+++ models/train.py
@@ -1,63 +1,30 @@
 from cgitb import reset
 import time
-import pickle
-from collections import deque # double ended queue (can append and pop in both end)
 import numpy as np
 import random
 import torch
 import copy
-
-
-def init_cache(INITIAL_EPSILON, REPLAY_MEMORY):
-    """initial variable caching, done only once"""
-    t, D = 0, deque(maxlen=REPLAY_MEMORY) # for experience replay
-    set_up_dict = {"epsilon": INITIAL_EPSILON, "step": t, "D": D, "highest_score": 0}
-    save_obj(set_up_dict, "set_up")
-
-def save_obj(obj, name):
-    with open('./result/'+ name + '.pkl', 'wb') as f: #dump files into objects folder
-        # Use the latest protocol that supports the lowest Python version you want to support reading the data
-        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
-        
-def load_obj(name):
-    with open('result/' + name + '.pkl', 'rb') as f:
-        return pickle.load(f)
-
-class AverageMeter:
-    ''' Computes and stores the average and current value '''
-    def __init__(self) -> None:
-        self.reset()
-
-    def reset(self) -> None:
-        self.val = 0.0
-        self.avg = 0.0
-        self.sum = 0.0
-        self.count = 0
-
-    def update(self, val: float, n: int = 1) -> None:
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
+import sys
+sys.path.append("../")
+from utils.utils import AverageMeter, save_obj
 
 class trainNetwork:
-    def __init__(self, agent,game, writer, Deque, BATCH, device):
+    def __init__(self, agent,game, writer, buffer, BATCH, device):
         self.agent = agent
         self.agent.online.to(device)
         self.agent.target.to(device)
         self.game = game
         self.device = device
         self.writer = writer
-        self.memory = Deque
+        self.memory = buffer
         self.batch_size = BATCH
+        self.eps = 1.0
 
     def cache(self, state, action_idx, reward, next_state, terminal):
         """
         Store the experience to self.memory (replay buffer)
         """
 
-        #state = state.__array__()
-        #next_state = next_state.__array__()
         state = state.detach().clone().cpu()
         action_idx = torch.tensor(action_idx).detach().clone().cpu()
         next_state = next_state.detach().clone().cpu()
@@ -67,37 +34,20 @@ class trainNetwork:
         self.memory.append((state, action_idx, reward, next_state, terminal))
 
 
-    def recall(self):
-        """
-        Retrieve a batch of experiences from memory
-        """
-        batch = random.sample(self.memory, self.batch_size)
-        state, action_idx, reward, next_state, terminal = map(torch.stack, zip(*batch))
-        state = torch.squeeze(state)
-        next_state = torch.squeeze(next_state)
-        reward = torch.squeeze(reward)
-        terminal = torch.squeeze(terminal)
-        return state.to(self.device), action_idx.to(self.device), reward.to(self.device), next_state.to(self.device), terminal.to(self.device)
+    def save(self, epsilon, step, highest_score):
+        self.game.pause() #pause game while saving to filesystem
+        print("Now we save model")
+        self.agent.save_model()
+        # set_up_dict = {"epsilon": epsilon, "step": step, "D": self.memory, "highest_score": highest_score}
+        # save_obj(set_up_dict, "set_up") # save the buffer to disk, need lots of space if the buffer size is large
+        self.game.resume()
 
-    def td_estimate(self, state, action):
-        pass
-
-    @torch.no_grad()
-    def td_target(self, reward, next_state, done):
-        pass
-
-    def update_Q_online(self, td_estimate, td_target):
-        pass
-    
-    def sync_Q_target(self):
-        pass
-
-    def save(self):
+    def early_stopping(self):
         pass
 
     def start(self, epsilon, step, highest_score, 
             OBSERVE, ACTIONS, EPSILON_DECAY, FINAL_EPSILON, GAMMA,
-            FRAME_PER_ACTION, EPISODE, SAVE_EVERY, SYNC_EVERY, TRAIN_EVERY):
+            FRAME_PER_ACTION, EPISODE, SAVE_EVERY, SYNC_EVERY, TRAIN_EVERY, prioritized_replay):
         last_time = time.time() # for computing fps
         current_episode = 0
         num_action_0 = 0
@@ -123,7 +73,7 @@ class trainNetwork:
             total_reward = 0
             start_time = time.time()
             while not self.game.get_crashed(): # not game over yet
-                time.sleep(.03) # make the fps lower for small batch number
+                #time.sleep(.03) # make the fps lower for small batch number
                 loss = torch.zeros(1)
                 Q_sa = torch.zeros((1,2))
                 action_idx = 0
@@ -157,7 +107,6 @@ class trainNetwork:
                 
                 # store the transition (experience tuple) in a deque for experience replay
                 self.cache(s_t, action_idx, r_t, s_t1, terminal)
-                
                 # only train for train =='train' or 'continue_train'
                 if step > OBSERVE:
                     # epsilon decay: reduce the chance to random explore 
@@ -165,33 +114,42 @@ class trainNetwork:
                     epsilon = max(FINAL_EPSILON, epsilon)
 
                     #sample a minibatch to train
-                    state_t, action_t, reward_t, state_t1, terminal = self.recall()
+                    if prioritized_replay:
+                        # prioritization exponent
+                        alpha = 0.7 # 0.7 was suggested by the prioritized_replay paper
+                        state_t, action_t, reward_t, state_t1, terminal, importance, indices =\
+                        self.memory.recall(self.batch_size, priority_scale=alpha)
+                    else:
+                        importance = None
+                        state_t, action_t, reward_t, state_t1, terminal = self.memory.recall(self.batch_size)
                     #td_target = torch.zeros((self.batch_size, ACTIONS)) # (batch_size, 2)
                     
                     if step % SYNC_EVERY == 0:
                         self.agent.sync_target()
 
                     if step % TRAIN_EVERY == 0:
-                        loss, avg_q_max= self.agent.step(state_t.to(self.device), action_t.to(self.device), reward_t.to(self.device), state_t1.to(self.device), terminal.to(self.device))
+                        if importance is not None: importance = importance.to(self.device)**(1-self.eps) # use prioritized buffer
+                        loss, avg_q_max, error = self.agent.step(state_t.to(self.device), action_t.to(self.device), reward_t.to(self.device),\
+                                                        state_t1.to(self.device), terminal.to(self.device),\
+                                                        importance)
+
+                        if prioritized_replay: self.memory.set_priorities(indices, error) # update the priorities of priorities buffer
+                        if torch.any(terminal): self.eps = max(0.1, 0.999*self.eps)
                         
                         # record the log
                         avg_loss.update(loss, self.batch_size)
                         avg_Q_max.update(avg_q_max, self.batch_size)
                         self.writer.add_scalar("Train/loss", avg_loss.avg, step)
-                    
+                        
+                    #################################################################################################################################
+
                     # save model
                     if step % SAVE_EVERY == 0:
-                        self.game.pause() #pause game while saving to filesystem
-                        print("Now we save model")
-                        self.agent.save_model()
-                        set_up_dict = {"epsilon": epsilon, "step": step, "D": self.memory, "highest_score": highest_score}
-                        save_obj(set_up_dict, "set_up")
-                        self.game.resume()
+                        self.save(epsilon, step, highest_score)
                 
                 s_t = copy.deepcopy(s_t1) # assign next state to current state
                 step += 1
                 
-            
             # update the log after the end of each episode
             current_episode += 1
 
@@ -203,8 +161,8 @@ class trainNetwork:
             self.writer.add_scalar("Train/reward", total_reward, current_episode)
             avg_score.update(current_score, 1)
             avg_reward.update(total_reward, 1)
-            print('Episode [{}/{}], Step {}, Epsilon {:.5f}, Action: (0:{},1:{}), Avg FPS: {:.4f},  Avg Reward: {:.4f}, Avg Q_MAX: {:.4f}, Avg Loss: {:.4f}, Hihgest Score: {}'
-                .format(current_episode, EPISODE, step, epsilon, num_action_0, num_action_1, avg_fps.avg,
+            print('Episode [{}/{}], Step {}, Epsilon: {:.5f}, EPS: {:.5f} Action: (0:{},1:{}), Avg FPS: {:.4f},  Avg Reward: {:.4f}, Avg Q_MAX: {:.4f}, Avg Loss: {:.4f}, Hihgest Score: {}'
+                .format(current_episode, EPISODE, step, epsilon, self.eps, num_action_0, num_action_1, avg_fps.avg,
                 avg_reward.avg, avg_Q_max.avg, avg_loss.avg, highest_score))
             
             avg_fps.reset()
